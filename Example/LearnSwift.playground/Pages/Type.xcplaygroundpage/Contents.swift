470a63181a5437e6d279ee2aa88716f45e39ff21
gaeasoft.eastroot <eastroot1590@gmail.com>
2021-04-13 15:40:47 +0900

    Swift 기본 자료형 v0.1

---
+++ Example/LearnSwift.playground/Pages/Type.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+var flag: Int = 5
+
+if flag != 0 {
+    print("flag is not zero")
+}
+
+// MARK: 정수
+print("Int64 max=\(Int64.max) min=\(Int64.min)")
+
+var sixfour: Int64 = 10
+var threetwo: Int32 = 5
+//sixfour = threetwo
+
+// MARK: 문자열
+let apple: Int = 10
+let banana: Int = 5
+
+var character: Character = "s"
+var string: String = character
+
+print("there are \(apple) apples and \(banana) bananas.")
+
+if flag != 0 {
+    let multiLine: String = """
+        hello
+        I
+        am
+        geaasoft
+    """
+    
+    print(multiLine)
+}
+
